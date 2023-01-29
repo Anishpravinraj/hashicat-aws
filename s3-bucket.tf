58800737f346d4af76c50a94c8c7067eada0e64a
Anishpravinraj <47388445+Anishpravinraj@users.noreply.github.com>
2023-01-28 22:44:09 -0600

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,12 +2,15 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  
   acl    = "private"
   bucket_prefix = "anishseelan"
 
   versioning = {
     enabled = true
   }
-
+  tags = {
+    Department = "IT"
+    Billable = "Yes"
+  }
 }
+
